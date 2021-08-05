9e57f94bccf759aff1b099282ca18038ff53d1fa
yuvraj3010 <45760033+yuvraj3010@users.noreply.github.com>
2021-08-05 19:31:15 +0530

    Update get_VM_IP.sh

--- get_VM_IP.sh
+++ get_VM_IP.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/bash
 
+Ip_Address=$(echo `az vm list-ip-addresses -g trainerrg1 -n testvm --query "[].virtualMachine.network.publicIpAddresses[*].ipAddress" -o tsv`)
 
+
+echo "[linux]" >> /etc/ansible/hosts
+echo "$Ip_Address" >> /etc/ansible/hosts
+echo "[linux:vars]" >> /etc/ansible/hosts
+echo "ansible_ssh_user = vmadmin" >> /etc/ansible/hosts
+echo "ansible_ssh_pass = P@ssword123456" >> /etc/ansible/hosts
